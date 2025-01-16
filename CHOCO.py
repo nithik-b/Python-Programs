6a625362ce57da43b03a2bd758d43cdc4f9c392b
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-16 22:52:09 +0530

    Delete CHOCO.py

--- CHOCO.py
+++
@@ -1,8 +0,0 @@
-def choco(n,A):
-    Sum = 0
-    for i in range(n):
-        Sum = Sum + A[i] * (i + 1) * (n - i)
-    return Sum
-n = int(input()) 
-A = list(map(int,input().split()))  
-print(choco(n,A))
