07c86fe62686f05ea38a6949e86e28446b776378
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-16 22:56:33 +0530

    Initial commit
    Adding comments to the program

--- Reatil bill.py
+++ Reatil bill.py
@@ -1,3 +1,5 @@
+# To create the retail bill for the fruit's shop
+# Setting the product's price
 pro_list={"Apple":50.0,
          "Banana":30.0,
          "Orange":40.0,
@@ -5,12 +7,14 @@ pro_list={"Apple":50.0,
          "Mango":100.0}
 print("Welcome to the Retail Store!")
 print("Product Catalog:")
+#The products and it's price is calculated here.
 for product,price in pro_list.items():
     print(f"{product}: {price:.1f}")
 pro_name=input("Enter the product name (or 'done' to finish):")
 if pro_name == "done":    
         print("Enter")
 
+# Creating function to choose the fruit's and their quantity
 def fin():
         a=int(input(f"Enter the quantity of {pro_name}:"))
         if pro_name == "Apple":
@@ -26,5 +30,6 @@ def fin():
         elif pro_name == "Mango":
             return pro_list[pro-name]*a
         print("Retail Bill")
+# Calling the function
 print(fin())    
             
