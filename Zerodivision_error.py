131f2c2887c53c171653a58821dc28ae061c7e43
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-16 22:59:59 +0530

    Initial commit

--- Zerodivision_error.py
+++ Zerodivision_error.py
@@ -1,7 +1,7 @@
-num = int(input("Enter the numerator:"))
-deno = int(input("Enter the denominator:"))
-try:
-    quo = num/deno
+num = int(input("Enter the numerator:"))    # Getting numberator from the user
+deno = int(input("Enter the denominator:"))  #Getting denominator from the user\
+try:                             # Try block
+    quo = num/deno               # Finding Quotient 
     print("QUOTIENT",quo)
-expect ZeroDivisionError:
+expect ZeroDivisionError:          # Except block executes when it throw the expection
     print("Denominator cannot be zero")
