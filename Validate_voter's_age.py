9bbd01801f2a6cb4bdf4ecfd814599bf31afbce8
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-17 08:42:05 +0530

    Initial commit

--- Validate_voter's_age.py
+++ Validate_voter's_age.py
@@ -1,9 +1,15 @@
+# Validating voter's age
+# try block
 try:
     age = int(input("Enter your age: "))
+    # Check the condition if age is less than 18 
+    # Raise exception if the conmdition is true
     if age < 18:
         raise ValueError("Invalid age! You must be at least 18 years old to vote.")
     print("You are eligible to vote!")
+# Exception block
 except ValueError as e:
     print(f"Error: {e}")
+# It will excecutes at last of the program.
 finally:
     print("Thank you for using the voter eligibility checker.")
