d81e19bb071bffef60ad34d17f5f93bcf54b40a8
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-18 08:24:21 +0530

    Initial commit

--- domain_of_email.py
+++ domain_of_email.py
@@ -1,7 +1,11 @@
+# Seperation of domain and username in email id 
+# Creating the user-defined function.
 def email(n):
     username,domain=n.split('@')
     return username, domain
+# Getting input from the user
 n=input("Enter an email address:")
+# Calling the function
 username,domain=email(n)
 print(f"Username: {username}")
 print(f"Domain: {domain}")
