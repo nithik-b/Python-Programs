5d1c919b1fee5883a0f2d45e09482520e54a2d4a
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-18 08:26:58 +0530

    Delete max_file.py

--- max_file.py
+++
@@ -1,7 +0,0 @@
-import sys
-numbers = [int(arg) for arg in sys.argv[1:]]
-max_num =numbers[0]
-for num in numbers:
-    if num > max_num:
-        max_num=num
-print("The maximum number in the list:",max_num)        
