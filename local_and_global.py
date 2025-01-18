b1630aa659ac7c296036c431f5318aed094f1dea
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-18 08:26:32 +0530

    Initial commit

--- local_and_global.py
+++ local_and_global.py
@@ -1,7 +1,9 @@
+# Using of local and global variable
 x=10
 def func():
     x=5
     print("The value of x inside function is",x)   #use of local variable
+# Calling the function
 func()
 print("The value of x outside the function",x)    #use of global variable
     
