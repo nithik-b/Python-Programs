102b3eb272981580a30d09076b4b4a83eec520b5
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-20 07:39:46 +0530

    Initial commit

--- Sum_of_List.py
+++ Sum_of_List.py
@@ -1,12 +1,16 @@
+# Performing sum of list.
+# Creating user-defined functions.
 def Sumelem(lst):
     Sum=0
     for a in lst:
         Sum = Sum+a
     return Sum
+# Getting list of numbers from the user.
 a=[]
 n=int(input("Enter how many numbers entered in list:"))
 for i in range(1,n+1):
     b=int(input(f"Enter a number {i}:"))
     a.append(b)
+# Calling the function inside the print statement.
 print("Sum of the given list:",Sumelem(a))    
     
