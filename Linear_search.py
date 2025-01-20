fda07224f2c08b644d5ddd9edd94e3bd59791066
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-20 07:49:48 +0530

    Initial commit

--- Linear_search.py
+++ Linear_search.py
@@ -1,3 +1,5 @@
+# Performing Linear search.
+# Creating the user-defined function
 def lin(c):
     for i in range(n):
         if a[i]==c:
@@ -5,10 +7,13 @@ def lin(c):
             break
     else:
         print("Element not found")
+# Getting list of numbers from the user.
 a=[]
 n=int(input("Enter how many numbers in list:"))
 for i in range(1,n+1):
-    b=int(input("Enter the number:"))
+    b=int(input(f"Enter the number {i}:"))
     a.append(b)
+print("The list:",a)
 c=int(input("Enter the value to be searched:"))
+# Calling the function
 lin(c)
