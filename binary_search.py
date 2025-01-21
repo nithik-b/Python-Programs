0ca94fe89d4ddeb26182b9721ad14c3bc1cd349b
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-21 13:20:15 +0530

    Initial commit

--- binary_search.py
+++ binary_search.py
@@ -1,25 +1,25 @@
-#Performing binary search
-#Creating user-defined function (binary(a,b))
+#Performing binary search,
+#Creating user-defined function (binary(a,b)),
 def binary(a,b):
     low=0
     high=n-1
-    # Check base case
+    # Check base case,
     while(low<=high):
         mid=(high+low)//2
-        # If element is present at the middle itself
+        # If element is present at the middle itself,
         if a[mid]==b:
             return mid
         # If element is smaller than mid, then it can only be present in left subarray.
         elif a[mid]>b:
             high=mid-1
-        # Else the element can only be present in right subarray
+        # Else the element can only be present in right subarray,
         else:
             low=mid+1
-    # Element is not present in the array
+    # Element is not present in the array,
     else:
         return -1
             
-# Getting list of numbers from the user
+# Getting list of numbers from the user,
 n=int(input("Enter how many numbers in list:"))        
 a=[]
 for i in range(1,n+1):
@@ -27,9 +27,9 @@ for i in range(1,n+1):
     a.append(b)
     a.sort()
 print(a)
-# Getting the key value from the user
+# Getting the key value from the user.
 c=int(input("Enter a number to be found:"))
-# Founding the element by calling the function
+# Founding the element by calling the function.
 d=binary(a,c)
 if d==-1:
     print("The element not found")
