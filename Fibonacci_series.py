78159903068fd89895333452c7818bb0399f35e1
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-22 08:37:05 +0530

    Initial commit

--- Fibonacci_series.py
+++ Fibonacci_series.py
@@ -1,10 +1,14 @@
+# Performing fibonacci series.
+# Creating user-defined functions.
 def fib(n):
   if n==0:
-    return 0
+    return 0        # Base case.
   elif n==1:
-    return 1
+    return 1        # Base case.
   else:
-    return fib(n-1) + fib(n-2)
+    return fib(n-1) + fib(n-2)        # Recursive case.
+# Getting input from the user.
 n=int(input("Enter a number:"))
+# Printing the value by using the for loop.
 for i in range(0,n+1):
   print("Fibonacci(",i,"):",fib(i))
