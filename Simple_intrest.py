a9325a9c9a43f25a66f9fb63246960bb3980b329
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-24 20:41:24 +0530

    Initial commit

--- Simple_intrest.py
+++ Simple_intrest.py
@@ -1,5 +1,7 @@
+# Performing simple intrest calculation. 
 p = float(input("Enter the principle amount:"))
 n = float(input("Enter the time in years:"))
 r = float(input("Enter the annual intrest rate(in percentage):"))/100
-simp_intr=p*n*r
+simp_intr=p*n*r     # Formula for simple intrest.
+# Printing simple intrest on following with two floating points.
 print(f"The simple intrest is {simp_intr:.2f}")
