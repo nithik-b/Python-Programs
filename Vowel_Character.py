8fe0970278ce2184699cfca49927dc25c73252b3
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-25 17:41:53 +0530

    Initial commit

---
+++ Vowel_Character.py
@@ -0,0 +1,7 @@
+# Finding the character if it is vowel.
+# Getting the input from the user.
+n=input("Enter a single character:")
+if n in ("a","e","i","o","u") or n in ("A","E","I","O","U"):
+  print("The given single character is vowel")
+else:
+  print("The given single character is consonants")
