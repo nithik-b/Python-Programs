f1ce56d218c1484159ec8e7a0d67754cc3825c7f
Nithikbalaji S <167744497+nithik-b@users.noreply.github.com>
2025-01-25 17:11:29 +0530

    Initial commit

--- Star_pattern.py
+++ Star_pattern.py
@@ -1,3 +1,6 @@
+# Performing star pattern.
+# Getting input from the user.
 n = int(input("Enter the number of rows: "))
+# By using for loop achieving star pattern. 
 for i in range(1,n+1):
   print(" "*(n-i)+"*"*(2*i-1))
